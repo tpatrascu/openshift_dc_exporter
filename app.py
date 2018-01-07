387d0a00dc2eca86409b61c87e226e3dc0339178
Tiberiu Patrascu <you@example.com>
2018-01-07 02:09:23 +0200

    typo

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ EXPORTER_PORT = 8080
 
 class DCCollector(object):
     def collect(self):
-        for dc in oapi.list_deployment_config_for_all_namespaces.items:
+        for dc in oapi.list_deployment_config_for_all_namespaces().items:
             dc_status = oapi.read_namespaced_deployment_config_status(dc.metadata.name, namespace)
 
             dc_metrics = {
